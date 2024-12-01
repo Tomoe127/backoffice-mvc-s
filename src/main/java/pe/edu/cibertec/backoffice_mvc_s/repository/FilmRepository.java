e447499a5323ddecf4f52462a3ab283656102158
Anthony Christian Ramos Rojas <eltanin.pe@gmail.com>
2024-11-30 19:24:40 -0500

    subiendo

---
+++ src/main/java/pe/edu/cibertec/backoffice_mvc_s/repository/FilmRepository.java
@@ -0,0 +1,7 @@
+package pe.edu.cibertec.backoffice_mvc_s.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import pe.edu.cibertec.backoffice_mvc_s.entity.Film;
+
+public interface FilmRepository extends CrudRepository<Film, Integer> {
+}
